904bdd66f2c4dc5d6c68541b455b4326bfa3d03c
الرفاعي للشحن الدولي <moabdalhai1995@gmail.com>
2026-08-14 15:23:22 +0300

    Create MainActivity.java

---
+++ app/java/com/alrifai/shipping/MainActivity.java
@@ -0,0 +1,27 @@
+package com.alrifai.shipping;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://alrifai-shipping-platform.onrender.com");
+    }
+}
