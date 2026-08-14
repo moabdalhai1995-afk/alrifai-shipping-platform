fc3fac7ab92e22ff159c2e87ece66385a3d51adf
الرفاعي للشحن الدولي <moabdalhai1995@gmail.com>
2026-08-14 15:16:54 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,15 @@
 package com.alrifai.shipping;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String PLATFORM_URL = "https://alrifai-shipping-platform.onrender.com";
-    private static final int FILE_CHOOSER_REQUEST = 1001;
-    private WebView webView;
-    private ValueCallback<Uri[]> fileCallback;
+    private static final String PLATFORM_URL =
+        "https://alrifai-shipping-platform.onrender.com";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -26,64 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        WebView webView = findViewById(R.id.webView);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                Uri uri = request.getUrl();
-                String scheme = uri.getScheme();
-                if ("http".equals(scheme) || "https".equals(scheme)) return false;
-                startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                return true;
-            }
-        });
-
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> callback,
-                                             FileChooserParams params) {
-                if (fileCallback != null) fileCallback.onReceiveValue(null);
-                fileCallback = callback;
-                try {
-                    startActivityForResult(params.createIntent(), FILE_CHOOSER_REQUEST);
-                    return true;
-                } catch (Exception error) {
-                    fileCallback = null;
-                    return false;
-                }
-            }
-        });
-
-        if (savedInstanceState == null) webView.loadUrl(PLATFORM_URL);
-        else webView.restoreState(savedInstanceState);
-
-        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            @Override public void handleOnBackPressed() {
-                if (webView.canGoBack()) webView.goBack();
-                else finish();
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FILE_CHOOSER_REQUEST && fileCallback != null) {
-            fileCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
-            fileCallback = null;
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        webView.saveState(outState);
-        super.onSaveInstanceState(outState);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(PLATFORM_URL);
     }
 }
