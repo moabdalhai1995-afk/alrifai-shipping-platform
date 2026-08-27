e9e3822d884762d39b0ab6ea8fb3ae2eac897e81
الرفاعي للشحن الدولي <moabdalhai1995@gmail.com>
2026-08-27 21:27:28 +0300

    Load package barcode service

--- reset-preload.js
+++ reset-preload.js
@@ -181,3 +181,4 @@ function wrappedExpress(...args) {
 
 Object.assign(wrappedExpress, realExpress);
 require.cache[require.resolve("express")].exports = wrappedExpress;
+require("./scripts/package-barcodes-preload.js");
